d91744124936559865890d3195558b3628802117
TomoeYasuda <tomoe.pierrot3@gmail.com>
2022-03-22 00:17:13 +0900

    Make Card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import s from './Card.module.css';
+
+
+const Card = () => {
+  return (
+    <>
+      <div className={s.navigationContainer}>
+        <div>
+        <h1>TOMOE</h1>
+        <ul>
+          <li><a>Profile</a></li>
+          <li><a>Product</a></li>
+          <li><a>Activity</a></li>
+          <li><a>Tech stack</a></li>
+        </ul>
+          </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import Header from 'src/components/Header'
+import Card from '@components/Card'
 
 const Home: NextPage = () => {
 
   return (
     <>
-      <h1>NUTMEG</h1>
+      <Card />
+       <Header />
+
     </>
   )
 }
