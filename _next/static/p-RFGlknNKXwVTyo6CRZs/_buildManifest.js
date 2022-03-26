ca1efce665b18ce7f49e905373de7ef80e03aa7f
TomoeYasuda <TomoeYasuda@users.noreply.github.com>
2022-03-26 11:43:14 +0000

    deploy: 4de1c4242ab62a687c2aaae14bea58eb4b661b7d

---
+++ _next/static/p-RFGlknNKXwVTyo6CRZs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f44808b23c743f03.css","static/chunks/pages/index-e6b41fd014eb6b4d.js"],"/_error":["static/chunks/pages/_error-1f95674706ce531a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
